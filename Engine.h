4062322b79895b8a8aa8000ea960000f96d9a643
xandercolin <68094157+xandercolin@users.noreply.github.com>
2020-07-13 21:59:17 -0500

    Add files via upload

---
+++ Engine.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "TextureHolder.h"
+#include <SFML/Graphics.hpp>
+#include "TextureHolder.cpp"
+
+using namespace sf;
+
+class Engine {
+private:
+	TextureHolder th;
+	const int TILE_SIZE = 50;
+	const int VERTS_IN_QUAD = 4;
+	const int Gravity = 300;
+	//a regular render window
+	RenderWindow m_Window;
+	View m_MainView;
+	View m_LeftView;
+	View m_RightView;
+	View m_BGMainView;
+	View m_BGLeftView;
+	View m_BGRightView;
+	View m_HudView;
+	Sprite m_BackgroundSprite;
+	Texture m_BackgroundTexture;
+	bool m_Playing = false;
+	bool m_Character1 = true;
+};
